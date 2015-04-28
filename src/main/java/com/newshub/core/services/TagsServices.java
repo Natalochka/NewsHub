d6ee980d2bd72c9824a93a3d2bb1ce4ddcfd7edb
Natalochka <natalochcka@yahoo.com>
2015-04-28 22:45:51 +0300

    update

--- src/main/java/com/newshub/core/services/TagsServices.java
+++ src/main/java/com/newshub/core/services/TagsServices.java
@@ -1,7 +1,16 @@
 package com.newshub.core.services;
 
+import com.newshub.core.domain.Tags;
+
 /**
  * Created by Natalie on 25.04.2015.
  */
 public class TagsServices {
+    public void addTag(){}
+    public void editTag(){}
+    public void deleteTag(){}
+    public Tags getTag(){
+        return null;
+    }
+
 }
--- src/main/java/com/newshub/core/services/UsersServices.java
+++ src/main/java/com/newshub/core/services/UsersServices.java
@@ -1,7 +1,17 @@
 package com.newshub.core.services;
 
+import com.newshub.core.domain.Users;
+
 /**
  * Created by Natalie on 25.04.2015.
  */
 public class UsersServices {
+    public void addUser(){}
+    public void changeUserPrivileges(){}
+    public void deleteUser(){}
+    public void editUserInfo(){}
+    public Users getUser(){
+        return null;
+    }
+
 }
