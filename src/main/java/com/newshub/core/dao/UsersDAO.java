ff0a7e40d247090b6acadeff934e64f7ab34672c
Natalochka <natalochcka@yahoo.com>
2015-04-29 17:10:03 +0300

    update

--- src/main/java/com/newshub/core/dao/UsersDAO.java
+++ src/main/java/com/newshub/core/dao/UsersDAO.java
@@ -1,24 +1,25 @@
 package com.newshub.core.dao;
 
+import com.newshub.core.domain.Privileges;
 import com.newshub.core.domain.Users;
 
 /**
  * Created by Natalie_2 on 4/28/2015.
  */
-public class UsersDAO implements DAO<Integer, Users> {
+public class UsersDAO implements DAO<Privileges, Users> {
     public void create(Users entity) {
 
     }
 
-    public void update(Integer id, Users entity) {
+    public void update(Privileges id, Users entity) {
 
     }
 
-    public void delete(Integer id) {
+    public void delete(Privileges id) {
 
     }
 
-    public Users get(Integer id) {
+    public Users get(Privileges id) {
         return null;
     }
 }
