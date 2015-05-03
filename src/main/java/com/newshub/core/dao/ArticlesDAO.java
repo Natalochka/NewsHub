b8bfb09d764844b137d94cf23cb15f38ef9f53f5
Natalochka <natalochcka@yahoo.com>
2015-05-04 00:49:27 +0300

    update

--- src/main/java/com/newshub/core/dao/ArticlesDAO.java
+++ src/main/java/com/newshub/core/dao/ArticlesDAO.java
@@ -1,6 +1,8 @@
 package com.newshub.core.dao;
 
 import com.newshub.core.domain.Articles;
+import com.newshub.core.utils.HibernateUtils;
+import org.hibernate.Session;
 
 /**
  * Created by Natalie_2 on 4/27/2015.
@@ -8,20 +10,71 @@ import com.newshub.core.domain.Articles;
 
 public class ArticlesDAO implements DAO<Integer, Articles>{
 
-    public void create (Articles entity) {}
+    public void create (Articles entity) {
+        Session session = null;
+        try {
+            session = new HibernateUtils().getSession();
+            session.beginTransaction();
+            session.save(entity);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+        } finally {
+            if (session != null && session.isOpen()) {
+
+                session.close();
+            }
+        }
+    }
 
     public void update (Articles entity){
+        Session session = null;
+        try {
+            session = new HibernateUtils().getSession();
+            session.beginTransaction();
+            session.update(entity);
+            session.getTransaction().commit();
+        }
+        catch (Exception e) {
+        } finally {
+            if (session != null && session.isOpen()) {
+
+                session.close();
+            }
+        }
     }
 
     public void delete (Integer id){
-/*Articles article = get(id);
         Session session = null;
-        session = new HibernateUtils().getSession();
-        session.delete(article);
-        session.getTransaction().commit();*/
+        try{
+            Articles article = get(id);
+            session = new HibernateUtils().getSession();
+            session.delete(article);
+            session.getTransaction().commit();
+        }
+        catch (Exception e) {
+        } finally {
+            if (session != null && session.isOpen()) {
+
+                session.close();
+            }
+        }
+
     }
 
     public Articles get (Integer id){
-        return null;
+        Session session = null;
+        Articles articles = null;
+        try{
+            session = new HibernateUtils().getSession();
+            articles = (Articles) session.load(Articles.class, id);
+        }
+        catch (Exception e) {
+        } finally {
+            if (session != null && session.isOpen()) {
+
+                session.close();
+            }
+        }
+        return articles;
     }
 }
---
+++ src/main/java/com/newshub/core/dao/UsersArticlesDAO.java
@@ -0,0 +1,25 @@
+package com.newshub.core.dao;
+
+import com.newshub.core.domain.UsersArticles;
+import com.newshub.core.domain.UsersArticlesPK;
+
+/**
+ * Created by Natalie_2 on 5/3/2015.
+ */
+public class UsersArticlesDAO implements DAO<UsersArticlesPK, UsersArticles> {
+    public void create(UsersArticles entity) {
+
+    }
+
+    public void update(UsersArticles entity) {
+
+    }
+
+    public void delete(UsersArticlesPK id) {
+
+    }
+
+    public UsersArticles get(UsersArticlesPK id) {
+        return null;
+    }
+}
