fb1b5e5eb568667337ed5b9e1a07fdce5af03b54
Rombicuboctahedron <edserbin@gmail.com>
2015-05-07 16:42:29 +0300

    Local "session" was deleted;  blocks "finally" were deleted; added variable "session" to classes.

--- src/main/java/com/newshub/core/dao/ArticlesDAO.java
+++ src/main/java/com/newshub/core/dao/ArticlesDAO.java
@@ -10,70 +10,53 @@ import org.hibernate.Session;
 
 public class ArticlesDAO implements DAO<Integer, Articles>{
 
+    private Session session;
+
     public void create (Articles entity) {
-        Session session = null;
         try {
-            session = new HibernateUtils().getSession();
+            //session = new HibernateUtils().getSession();
             session.beginTransaction();
             session.save(entity);
             session.getTransaction().commit();
         } catch (Exception e) {
-        } finally {
+        } /*finally {
             if (session != null && session.isOpen()) {
 
                 session.close();
             }
-        }
+        }*/
     }
 
     public void update (Articles entity){
-        Session session = null;
+
         try {
-            session = new HibernateUtils().getSession();
+
             session.beginTransaction();
             session.update(entity);
             session.getTransaction().commit();
         }
         catch (Exception e) {
-        } finally {
-            if (session != null && session.isOpen()) {
-
-                session.close();
-            }
         }
     }
 
     public void delete (Integer id){
-        Session session = null;
+
         try{
             Articles article = get(id);
-            session = new HibernateUtils().getSession();
             session.delete(article);
             session.getTransaction().commit();
         }
         catch (Exception e) {
-        } finally {
-            if (session != null && session.isOpen()) {
-
-                session.close();
-            }
         }
 
     }
 
     public Articles get (Integer id){
-        Session session = null;
         Articles articles = null;
         try{
-            session = new HibernateUtils().getSession();
             articles = (Articles) session.load(Articles.class, id);
         }
         catch (Exception e) {
-        } finally {
-            if (session != null && session.isOpen()) {
-
-                session.close();
-            }
         }
         return articles;
     }
--- src/main/java/com/newshub/core/dao/UsersDAO.java
+++ src/main/java/com/newshub/core/dao/UsersDAO.java
@@ -11,74 +11,49 @@ import javax.swing.*;
  * Created by Natalie_2 on 4/28/2015.
  */
 public class UsersDAO implements DAO<Integer, Users> {
+
+    private Session session;
+
     public void create(Users entity) {
-        Session session = null;
         try {
-            session = new HibernateUtils().getSession();
             session.beginTransaction();
             session.save(entity);
             session.getTransaction().commit();
         } catch (Exception e) {
 
-        } finally {
-            if (session != null && session.isOpen()) {
-
-                session.close();
-            }
         }
     }
 
     public void update(Users entity) {
-        Session session = null;
         try {
-            session = new HibernateUtils().getSession();
             session.beginTransaction();
             session.update(entity);
             session.getTransaction().commit();
         }
         catch (Exception e) {
 
-        } finally {
-            if (session != null && session.isOpen()) {
-
-                session.close();
-            }
         }
     }
 
     public void delete(Integer id) {
-        Session session = null;
         try{
             Users users = get(id);
-            session = new HibernateUtils().getSession();
             session.delete(users);
             session.getTransaction().commit();
         }
         catch (Exception e) {
 
-        } finally {
-            if (session != null && session.isOpen()) {
-
-                session.close();
-            }
         }
 
     }
 
     public Users get(Integer id) {
-        Session session = null;
         Users users = null;
         try{
-            session = new HibernateUtils().getSession();
             users = (Users) session.load(Users.class, id);
         }
         catch (Exception e) {
 
-        } finally {
-            if (session != null && session.isOpen()) {
-
-                session.close();
-            }
         }
         return users;
     }
