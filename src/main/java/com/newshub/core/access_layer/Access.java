daa0f97e63f62515820842be15566bf0e08c4fae
Natalochka <natalochcka@yahoo.com>
2015-05-09 23:02:52 +0300

    update

---
+++ src/main/java/com/newshub/core/access_layer/Access.java
@@ -0,0 +1,189 @@
+package com.newshub.core.access_layer;
+
+import com.newshub.core.domain.Articles;
+import com.newshub.core.domain.Privileges;
+import com.newshub.core.domain.Tags;
+import com.newshub.core.domain.Users;
+import com.newshub.core.services.ArticlesServices;
+import com.newshub.core.services.TagsServices;
+import com.newshub.core.services.UsersServices;
+import com.newshub.core.utils.HibernateUtils;
+import org.hibernate.Session;
+
+import java.util.List;
+
+/**
+ * Created by Natalie on 25.04.2015.
+ */
+public class Access {
+    private Session session;
+    private ArticlesServices articlesServices = new ArticlesServices(session);
+    private TagsServices tagsServices = new TagsServices(session);
+    private UsersServices usersServices = new UsersServices(session);
+    private Users user;
+    private Privileges privilege;
+
+    public Access() {
+        session = new HibernateUtils().getSession();
+    }
+
+    public void grantPrivileges(int id) {
+
+    }
+
+    public boolean connect(String login, String password) {
+
+        for (Users user : usersServices.getAllUsers()) {
+            if (user.getLogin().equals(login) && user.getPassword().equals(password)) {
+                this.user = user;
+                privilege = user.getPrivilegesByPrivilegeId();
+            }
+        }
+        return false;
+    }
+
+    public void addArticle(int id, String title, String content) {
+        if (privilege.getAddArticle()) {
+            articlesServices.addArticle(id, title, content);
+        }
+    }
+
+    public void editArticle(int id, String title, String content) {
+        if (privilege.getEditArticle()) {
+            articlesServices.editArticle(id, title, content);
+        }
+    }
+
+    public void checkArticle(int id, Boolean flag) {
+        if (privilege.getCheckArticle()) {
+            articlesServices.checkArticle(id, flag);
+        }
+    }
+
+    public void approveArticle(int id, Boolean flag) {
+        if (privilege.getApproveArticle()) {
+            articlesServices.approveArticle(id, flag);
+        }
+    }
+
+    public void archiveArticle(int id, Boolean flag) {
+        if (privilege.getArchiveArticle()) {
+            articlesServices.archiveArticle(id, flag);
+        }
+    }
+
+    public void featureArticle(int id, Boolean flag) {
+        if (privilege.getFeatureArticle()) {
+            articlesServices.featureArticle(id, flag);
+        }
+    }
+
+    public void deleteArticle(int id) {
+        if (privilege.getDeleteArticle()) {
+            articlesServices.deleteArticle(id);
+        }
+    }
+
+    public Articles getArticle(int id) {
+        if (privilege.getGetArticle()) {
+            return articlesServices.getArticle(id);
+        }
+        return null;
+    }
+
+    public List<Articles> getAllArticles(){
+        if (privilege.getGetAllArticles()) {
+            return articlesServices.getAllArticles();
+        }
+        return null;
+    }
+
+    public void addTag(int id, String name) {
+        if (privilege.getAddTag()) {
+            tagsServices.addTag(id, name);
+        }
+    }
+
+    public void addTagToArticle(int tagId, int articleId) {
+        if (privilege.getAddTagToArticle()) {
+            tagsServices.addTagToArticle(tagId, articleId);
+        }
+    }
+
+    public void editTag(int tagId, String tagName) {
+        if (privilege.getEditTag()) {
+            tagsServices.editTag(tagId, tagName);
+        }
+    }
+
+    public void deleteTag(int tagId, int articleId) {
+        if (privilege.getDeleteTag()) {
+            tagsServices.deleteTag(tagId, articleId);
+        }
+    }
+
+    public Tags getTag(int id){
+        if (privilege.getGetTag()) {
+            return tagsServices.getTag(id);
+        }
+        return null;
+    }
+
+    public List<Tags> getAllTags(){
+        if(privilege.getGetAllTags()){
+            return tagsServices.getAllTags();
+        }
+        return null;
+    }
+
+    public List<Articles> getArticlesByTagId(int tagId){
+        if (privilege.getGetArticlesByTagId()) {
+            return tagsServices.getArticlesByTagId(tagId);
+        }
+        return null;
+    }
+
+    public void addUser(int privilegeId, int id, String login, String password, String email, String firstName, String lastName) {
+        if (privilege.getAddUser()) {
+            usersServices.addUser(privilegeId, id, login, password, email, firstName, lastName);
+        }
+    }
+
+    public void changeUserPrivileges(int privilegesId, int id) {
+        if (privilege.getChangeUserPrivileges()) {
+            usersServices.changeUserPrivileges(privilegesId, id);
+        }
+    }
+
+    public void deleteUser(int id) {
+        if (privilege.getDeleteUser()) {
+            usersServices.deleteUser(id);
+        }
+    }
+
+    public void editUserInfo(int id, String login, String password, String email, String firstName, String lastName) {
+        if (privilege.getEditUserInfo()) {
+            usersServices.editUserInfo(id, login, password, email, firstName, lastName);
+        }
+    }
+
+    public Users getUser(int id) {
+        if (privilege.getGetUser()) {
+            return usersServices.getUser(id);
+        }
+        return null;
+    }
+
+    public List<Users> getAllUsers() {
+        if (privilege.getGetAllUsers()) {
+            return usersServices.getAllUsers();
+        }
+        return null;
+    }
+
+    public void close() {
+        user = null;
+        privilege = null;
+    }
+
+}
